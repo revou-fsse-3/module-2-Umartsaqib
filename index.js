43e3774e6613a097ea43a0d2915738fd1775ed48
Umartsaqib <umartsqb@gmail.com>
2023-11-10 08:52:16 +0700

    module 2 advance assignment

--- index.js
+++ index.js
@@ -65,3 +65,30 @@ function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+
+const isNightMode = localStorage.getItem('nightMode');
+
+if (isNightMode === 'enabled') {
+  enableNightMode();
+}
+
+function enableNightMode() {
+  const body = document.body;
+  body.classList.add('night-mode');
+  localStorage.setItem('nightMode', 'enabled');
+}
+
+function disableNightMode() {
+  const body = document.body;
+  body.classList.remove('night-mode');
+  localStorage.setItem('nightMode', 'disabled');
+}
+
+document.getElementById('night-mode-button').addEventListener('click', function() {
+  const isNightModeEnabled = document.body.classList.contains('night-mode');
+  if (isNightModeEnabled) {
+    disableNightMode();
+  } else {
+    enableNightMode();
+  }
+});
